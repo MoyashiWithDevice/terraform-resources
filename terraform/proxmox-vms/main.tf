a2a72bb8ef52f816f311a6a12bb6c282f43ef781
bababa <pijonsan@gmail.com>
2026-01-20 10:42:38 +0900

    マシン作成

--- terraform/proxmox-vms/main.tf
+++ terraform/proxmox-vms/main.tf
@@ -1,6 +1,31 @@
-# ノード一覧を取得するだけのデータソース（VMはまだ作らない）
-data "proxmox_nodes" "all" {}
+# terraform/proxmox-vms/main.tf
 
-output "nodes" {
-  value = data.proxmox_nodes.all.names
+resource "proxmox_vm_qemu" "web_server" {
+  count       = 1
+  name        = "almalinux-${count.index}"
+  target_node = "pve" # Proxmoxのノード名
+
+  # クローン元のテンプレート名（あらかじめProxmoxで作っておく必要があります）
+  clone = "Ubuntu24.04-template"
+
+  # 基本スペック
+  cores   = 2
+  memory  = 2048
+
+  # ディスク設定
+  disk {
+    size    = "20G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+
+  # ネットワーク設定
+  network {
+    model  = "virtio"
+    bridge = "vmbr31"
+  }
+
+  # Cloud-init設定（IP固定やSSHキーなど）
+  os_type = "cloud-init"
+  ipconfig0 = "ip=172.31.0.13${count.index}/24,gw=172.31.0.254"
 }
