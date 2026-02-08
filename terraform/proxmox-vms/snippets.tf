05d0d8f6e36be8739ae0b66b1db9c33199e2c976
bababa <pijonsan@gmail.com>
2026-02-08 14:51:41 +0900

    fix

---
+++ terraform/proxmox-vms/snippets.tf
@@ -0,0 +1,27 @@
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local" # スニペットを保存するストレージ名
+  node_name    = var.target_node
+
+  source_raw {
+    file_name = "setup-cloud-config.yaml"
+    data      = <<EOF
+#cloud-config
+users:
+  - name: ansible
+    groups: sudo
+    shell: /bin/bash
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    ssh_authorized_keys:
+      - ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL/kEsjJ+fb3432waZDbiTvLIwG+0pVNc4WAG179rVDqzDeX6xahWJu9taWZY1hszJuf8f1RMzBW7WHjrQ7M17s= bababa@k8s
+
+package_update: true
+packages:
+  - qemu-guest-agent
+
+runcmd:
+  - systemctl enable qemu-guest-agent
+  - systemctl start qemu-guest-agent
+EOF
+  }
+}
--- terraform/proxmox-vms/vm_test.tf
+++ terraform/proxmox-vms/vm_test.tf
@@ -32,36 +32,8 @@ resource "proxmox_virtual_environment_vm" "test_server" {
       }
     }
   }
+  wait_for_network_config = true
   agent {
     enabled = true
-  }
-}
-
-# 1. Cloud-initの設定をファイルとして定義し、Proxmoxにアップロード
-resource "proxmox_virtual_environment_file" "cloud_config" {
-  content_type = "snippets"
-  datastore_id = "local" # スニペットを保存するストレージ名
-  node_name    = var.target_node
-
-  source_raw {
-    file_name = "setup-cloud-config.yaml"
-    data      = <<EOF
-#cloud-config
-users:
-  - name: ansible
-    groups: sudo
-    shell: /bin/bash
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    ssh_authorized_keys:
-      - ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBL/kEsjJ+fb3432waZDbiTvLIwG+0pVNc4WAG179rVDqzDeX6xahWJu9taWZY1hszJuf8f1RMzBW7WHjrQ7M17s= bababa@k8s
-
-package_update: true
-packages:
-  - qemu-guest-agent
-
-runcmd:
-  - systemctl enable qemu-guest-agent
-  - systemctl start qemu-guest-agent
-EOF
   }
 }
