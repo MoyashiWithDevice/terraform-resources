4fe0951ef81c40e7439e51ca9aa04a5e9ea396f4
bababa <pijonsan@gmail.com>
2026-02-20 18:59:34 +0900

    LB用マシンを作成

---
+++ terraform/proxmox-vms/vm_LB.tf
@@ -0,0 +1,34 @@
+# __generated__ by Terraform
+resource "proxmox_virtual_environment_vm" "load_balancer" {
+  name                                 = "lb-${count.index}"
+  node_name                            = "lb"
+  vm_id                                = 110+ count.index
+  cpu{
+    cores = 4
+  }
+  memory{
+    dedicated = 4096
+  }
+
+  network_device {
+    model = "virtio"
+    bridge = "vmbr32"
+  }
+  
+  initialization {
+    datastore_id = "local-lvm"
+    interface = "ide2"
+
+    ip_config{
+      ipv4{
+        address = "172.32.0.${100 + count.index}/24"
+        gateway = "172.32.0.254"
+      }
+    }
+    
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+  }  
+  agent{
+    enabled = true
+  }
+}
