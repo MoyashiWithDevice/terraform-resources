08392f5b0fd00bceba8dba7966ef845c4bd6aa0a
bababa <pijonsan@gmail.com>
2026-03-11 15:08:09 +0900

    add machine

---
+++ terraform/proxmox-vms/other.tf
@@ -0,0 +1,42 @@
+# terraform/proxmox-vms/vm_test.tf
+resource "proxmox_virtual_environment_vm" "lent-haruto-1" {
+  name        = "lent-haruto-1"
+  vm_id = 110
+  node_name = var.target_node
+
+  clone{
+    vm_id = var.template_vmid
+    full = true
+  }
+  cpu{
+    cores = 3
+  }
+  memory{
+    dedicated = 4096
+  }
+
+  network_device {
+    model = "virtio"
+    bridge = "vmbr32"
+  }
+
+
+  boot_order = ["scsi0"]
+
+  initialization {
+    datastore_id = "local-lvm"
+    interface = "ide2"
+    
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+
+    ip_config{
+      ipv4{
+        address = "172.32.0.15/24"
+        gateway = "172.32.0.254"
+      }
+    }
+  }
+  agent {
+    enabled = true
+  }
+}
